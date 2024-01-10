e651f558401cfd3ef18bf46f8a9398f2f4499a57
kirellkekw <arda1033@gmail.com>
2024-01-10 19:28:19 +0300

    removed unnecessary code

--- check_file_age.py
+++ check_file_age.py
@@ -5,19 +5,15 @@ from config import *
 
 
 async def purge_old_files():
+    print("File purge subprocess started.")
     while True:
         await asyncio.sleep(60)  # every minute
         for file in os.listdir(download_directory):
-            last_change = os.path.getmtime(
-                os.path.join(download_directory, file))
+            # get creation time of file
+            last_change = os.path.getctime(os.path.join(download_directory, file))
             now = time.time()
             file_age = int(now - last_change)
-            # print(file_age)
+            # print(file, file_age)
             if file_age > max_file_age:
+                print(f"Deleting {file}, {file_age} seconds old")
                 os.remove(os.path.join(download_directory, file))
-
-
-async def main():
-    await asyncio.gather(purge_old_files())
-
-asyncio.run(main())
