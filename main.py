2229243cd66d4432df35e2feaccd0bd77fd499af
arda <arda1033@gmail.com>
2024-03-20 22:40:14 +0300

    extra info on comments

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
         host="0.0.0.0",  # listen on all interfaces, won't matter since it's running in a container
         port=port,  # check config.yaml for the port
         reload=False,  # reload when code changes, useful for development, disable otherwise
-        log_level="debug",  # log level, change to "debug" for more verbose logging
+        log_level="debug",  # log level, "info" or "debug" recommended, "error" for production
         loop="asyncio",  # use asyncio event loop for sub-processes
     )
